ccd2b17de622ab8c50bc6039b23c0409b9d87ef3
alexeyzakirov <zakirov.alexey@gmail.com>
2024-05-01 10:46:24 +0300

    homework 6

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from catalog.views import ProductsListView, ProductDetailView, ContactsTemplateView
+from catalog.views import ProductsListView, ProductDetailView, ContactsTemplateView, ProductCreateView, ProductUpdateView, VersionCreateView, VersionListView, VersionUpdateView, VersionDetailView
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
@@ -7,5 +7,11 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductsListView.as_view(), name='index'),
     path('contacts/', ContactsTemplateView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('create', ProductCreateView.as_view(), name='create_product'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('version/', VersionCreateView.as_view(), name='version_create'),
+    path('versions/<int:pk>', VersionListView.as_view(), name='versions'),
+    path('version/<int:pk>/update/', VersionUpdateView.as_view(), name='version_update'),
+    path('version/<int:pk>', VersionDetailView.as_view(), name='version_detail')
 ]
